bbb85c90a4ef920d556af32640d2f9f518ae6f05
Eric C. Fields <fieldsec@westminster.edu>
2023-11-06 17:21:05 -0500

    update fix files code

---
+++ code/EmCon_fix_files.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Fix psychopy file naming problems for EmCon
+
+Author: Eric Fields
+Version Date: 6 November 2023
+"""
+
+import os
+from os.path import join
+import shutil
+
+import pandas as pd
+
+main_dir = r'C:\Users\fieldsec\OneDrive - Westminster College\Documents\ECF\Research\EmCon\DATA'
+behav_dir = join(main_dir, 'psychopy')
+
+#%% Copy files with repeated study name corrected
+
+for file in os.listdir(join(behav_dir, 'orig')):
+    
+    if file.startswith('06_EmCon_enc') and file.endswith('.csv'):
+        continue
+    
+    #Get rid of repeated study name
+    if 'EmCon_EmCon' in file:
+        new_file = join(behav_dir, file.replace('EmCon_EmCon', 'EmCon'))
+    else:
+        new_file = join(behav_dir, file)
+    
+    #Copy file with corrected name
+    shutil.copy2(join(behav_dir, 'orig', file), new_file)
+    
+
+#%% Combine encoding for 06_EmCon
+
+pt1 = pd.read_csv(join(behav_dir, 'orig', '06_EmCon_enc_2023-10-30_15h36.09.271.csv'))
+pt1 = pt1[pt1['valence'].notna()]
+pt2 = pd.read_csv(join(behav_dir, 'orig', '06_EmCon_enc_2023-10-30_16h01.42.053.csv'))
+pt2 = pt2[pt2['valence'].notna()]
+
+full_data = pd.concat((pt1, pt2), ignore_index=True)
+
+full_data.to_csv(join(behav_dir, '06_EmCon_enc_2023-10-30.csv'), index=False)
