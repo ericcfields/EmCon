3141d6f8ff6829b5f22ce6e2a330f7d7e113a266
Eric C. Fields <fieldsec@westminster.edu>
2024-03-10 16:46:24 -0400

    update mediation analysis script

--- stats/erp/avg/EmCon_MediationAnalysis.R
+++ stats/erp/avg/EmCon_MediationAnalysis.R
@@ -3,11 +3,19 @@
 #Author: Eric Fields
 #Version Date: 10 March 2024
 
+library(moments)
 library(stringr)
 library(readr)
 library(tibble)
 library(lme4)
 library(mediation)
+
+#Load Wilcox robust functions
+#Wilcox, R. R. (2016). Introduction to Robust Estimation and Hypothesis Testing (4th ed.). Waltham, MA: Elsevier.
+#https://dornsife.usc.edu/labs/rwilcox/software/
+if (!exists("yuen")) {
+  source("C:/Users/fieldsec/OneDrive - Westminster College/Documents/ECF/Coding/R/Rallfun-v43.txt")
+}
 
 
 setwd("C:/Users/fieldsec/OneDrive - Westminster College/Documents/ECF/Research/EmCon/DATA/stats/erp/avg")
@@ -48,6 +56,68 @@ tdata$delay <- factor(tdata$delay, levels=c("immediate", "delayed"))
 contrasts(tdata$delay) <- contr.simple(nlevels(tdata$delay))
 
 
+################################### GENERAL SETTINGS ###################################
+
+#Number of simulations to run when calculating inferential stats for mediation models
+sims <- 1e3
+
+#Output results to file
+sink("results/EmCon_mediation_results.txt")
+
+
+################################### DESCRIPTIVES ###################################
+
+DVs <- c("old_resp", "rk_resp", "frontal_pos", "LPP", "sub_bias", "N_trials")
+
+w_desc_table <- data.frame()
+for (DV in DVs) {
+  for (val in c("NEU", "NEG")) {
+    for (dly in c("immediate", "delayed")) {
+      dat <- wdata[wdata$valence==val & wdata$delay==dly, ][[DV]]
+      rowname <- sprintf("%s: %s %s", DV, val, dly)
+      w_desc_table[rowname, "M"] <- mean(dat)
+      w_desc_table[rowname, "SD"] <- sd(dat)
+      w_desc_table[rowname, "skew"] <- skewness(dat)
+      w_desc_table[rowname, "kurtosis"] <- kurtosis(dat) - 3
+      w_desc_table[rowname, "min"] <- min(dat)
+      w_desc_table[rowname, "25th"] <- quantile(dat, .25)
+      w_desc_table[rowname, "median"] <- median(dat)
+      w_desc_table[rowname, "75th"] <- quantile(dat, .75)
+      w_desc_table[rowname, "max"] <- max(dat)
+      w_desc_table[rowname, "trim_mean"] <- mean(dat, trim=0.2)
+      w_desc_table[rowname, "sw"] <- winsd(dat,tr=0.2)
+      w_desc_table[rowname, "MAD"] <- mad(dat)
+    }
+  }
+}
+
+write.csv(w_desc_table, "results/EmCon_mediation_WordAveraged_descriptives.csv")
+
+s_desc_table <- data.frame()
+for (DV in DVs) {
+  for (val in c("NEU", "NEG")) {
+    for (dly in c("immediate", "delayed")) {
+      dat <- sdata[sdata$valence==val & sdata$delay==dly, ][[DV]]
+      rowname <- sprintf("%s: %s %s", DV, val, dly)
+      s_desc_table[rowname, "M"] <- mean(dat)
+      s_desc_table[rowname, "SD"] <- sd(dat)
+      s_desc_table[rowname, "skew"] <- skewness(dat)
+      s_desc_table[rowname, "kurtosis"] <- kurtosis(dat) - 3
+      s_desc_table[rowname, "min"] <- min(dat)
+      s_desc_table[rowname, "25th"] <- quantile(dat, .25)
+      s_desc_table[rowname, "median"] <- median(dat)
+      s_desc_table[rowname, "75th"] <- quantile(dat, .75)
+      s_desc_table[rowname, "max"] <- max(dat)
+      s_desc_table[rowname, "trim_mean"] <- mean(dat, trim=0.2)
+      s_desc_table[rowname, "sw"] <- winsd(dat,tr=0.2)
+      s_desc_table[rowname, "MAD"] <- mad(dat)
+    }
+  }
+}
+
+write.csv(s_desc_table, "results/EmCon_mediation_SubAveraged_descriptives.csv")
+
+
 ########################## WORD AVERAGED MEDIATION ANALYSIS ##########################
 
 #Mediation separately in immediate and delayed conditions
@@ -59,16 +129,54 @@ for (dly in c("immediate", "delayed")) {
   contrasts(data_subset$valence) <- contr.simple(nlevels(data_subset$valence))
   
   #Calculate mediation
+  val.fit <- lm(old_resp ~ 1 + valence, data=data_subset)
+  lpp.fit <- lm(old_resp ~ 1 + LPP, data=data_subset)
   med.fit <- lm(LPP ~ 1 + valence, data=data_subset)
   out.fit <- lm(old_resp ~ 1 + LPP + valence, data=data_subset)
-  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP", 
-                     robustSE = TRUE, sims = 10000)
+  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP",
+                     control.value = "NEU", treat.value = "NEG",
+                     sims = sims, boot = TRUE, boot.ci.type = "bca")
   
   #Produce output in console and plots
-  cat(sprintf("\n\n%s WORD AVERAGED\n", str_to_upper(dly)))
+  cat(sprintf("\n\n\n####### %s WORD AVERAGED #######\n\n", str_to_upper(dly)))
+  print(summary(med.fit))
+  cat("\n\n")
+  print(summary(lpp.fit))
+  cat("\n\n")
+  print(summary(val.fit))
+  cat("\n\n")
+  print(summary(out.fit))
+  cat("\n\n")
   print(summary(med.out))
   plot(med.out)
   title(sprintf("%s WORD AVERAGED", str_to_upper(dly)))
+  
+  #Output results to file
+  #TO DO
+  
+  #Calculate mediation with response bias controlled
+  #QUESTION: sub_bias in med model?
+  val.fit <- lm(old_resp ~ 1 + valence + sub_bias, data=data_subset)
+  #lpp.fit <- lm(old_resp ~ 1 + LPP, data=data_subset)
+  med.fit <- lm(LPP ~ 1 + valence, data=data_subset)
+  out.fit <- lm(old_resp ~ 1 + LPP + valence + sub_bias, data=data_subset)
+  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP",
+                     control.value = "NEU", treat.value = "NEG",
+                     sims = sims, boot = TRUE, boot.ci.type = "bca")
+  
+  #Produce output in console and plots
+  cat(sprintf("\n\n\n####### %s WORD AVERAGED (CONTROLLING FOR BIAS) #######\n\n", str_to_upper(dly)))
+  print(summary(med.fit))
+  cat("\n\n")
+  print(summary(lpp.fit))
+  cat("\n\n")
+  print(summary(val.fit))
+  cat("\n\n")
+  print(summary(out.fit))
+  cat("\n\n")
+  print(summary(med.out))
+  plot(med.out)
+  title(sprintf("%s WORD AVERAGED (CONTROLLING FOR BIAS)", str_to_upper(dly)))
   
   #Output results to file
   #TO DO
@@ -86,15 +194,26 @@ for (dly in c("immediate", "delayed")) {
   contrasts(data_subset$valence) <- contr.simple(nlevels(data_subset$valence))
   
   #Calculate mediation
+  val.fit <- lmer(old_resp ~ 1 + valence + (1|sub_id), data=data_subset)
+  lpp.fit <- lmer(old_resp ~ 1 + LPP + (1|sub_id), data=data_subset)
   med.fit <- lmer(LPP ~ 1 + valence + (1|sub_id),
                   data=data_subset)
   out.fit <- lmer(old_resp ~ 1 + valence + LPP + (1|sub_id),
                   data=data_subset)
-  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP", 
-                     robustSE = TRUE, sims = 10000)
+  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP",
+                     control.value = "NEU", treat.value = "NEG",
+                     sims = sims)
   
   #Produce output in console and plots
-  cat(sprintf("\n\n%s SUBJECT AVERAGED\n", str_to_upper(dly)))
+  cat(sprintf("\n\n\n####### %s SUBJECT AVERAGED #######\n\n", str_to_upper(dly)))
+  print(summary(med.fit))
+  cat("\n\n")
+  print(summary(lpp.fit))
+  cat("\n\n")
+  print(summary(val.fit))
+  cat("\n\n")
+  print(summary(out.fit))
+  cat("\n\n")
   print(summary(med.out))
   plot(med.out)
   title(sprintf("%s SUBJECT AVERAGED", str_to_upper(dly)))
@@ -102,8 +221,38 @@ for (dly in c("immediate", "delayed")) {
   #Output results to file
   #TO DO
   
+  #Calculate mediation with response bias controlled
+  #QUESTION: sub_bias in med model?
+  val.fit <- lmer(old_resp ~ 1 + valence + sub_bias + (1|sub_id), data=data_subset)
+  #lpp.fit <- lmer(old_resp ~ 1 + LPP + (1|sub_id), data=data_subset)
+  med.fit <- lmer(LPP ~ 1 + valence + (1|sub_id),
+                  data=data_subset)
+  out.fit <- lmer(old_resp ~ 1 + valence + LPP + sub_bias + (1|sub_id),
+                  data=data_subset)
+  med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP",
+                     control.value = "NEU", treat.value = "NEG",
+                     sims = sims)
+  
+  #Produce output in console and plots
+  cat(sprintf("\n\n\n####### %s SUBJECT AVERAGED (CONTROLLING FOR BIAS) #######\n\n", str_to_upper(dly)))
+  print(summary(med.fit))
+  cat("\n\n")
+  print(summary(lpp.fit))
+  cat("\n\n")
+  print(summary(val.fit))
+  cat("\n\n")
+  print(summary(out.fit))
+  cat("\n\n")
+  print(summary(med.out))
+  plot(med.out)
+  title(sprintf("%s SUBJECT AVERAGED (CONTROLLING FOR BIAS)", str_to_upper(dly)))
+  
+  #Output results to file
+  #TO DO
+  
 }
 
+sink()
 stop("Code after this point does not work.")
 
 
@@ -124,7 +273,7 @@ for (dly in c("immediate", "delayed")) {
   #DOES NOT WORK AFTER THIS POINT
   #CANNOT HAVE TWO RANDOM FACTORS?
   med.out <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP", 
-                     robustSE = TRUE, sims = 2)
+                     robustSE = TRUE, sims = sims, boot = boot, boot.ci.type = boot.ci.type)
   
 }
 
@@ -144,11 +293,13 @@ med.fit <- lmer(LPP.c ~ 1 + valence*delay + (1|word),
 out.fit <- lmer(old_resp ~ 1 + valence*delay + delay*LPP.c + (1|word), 
                 data=data_subset)
 
-med.init <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP.c", sims=2)
+med.init <- mediate(med.fit, out.fit, treat = "valence", mediator = "LPP.c", 
+                    sims=2)
 
 #NOTE: DOES NOT WORK AFTER THIS POINT
 #NO MOEDRATED MEDIATION FOR MIXED MODELS?
 #Test moderation
 test.modmed(med.init, covariates.1 = list(delay = "immediate"),
-            covariates.2 = list(delay = "delay"), sims = 1000)
+            covariates.2 = list(delay = "delay"), 
+            sims = sims, boot = boot, boot.ci.type = boot.ci.type)
 
